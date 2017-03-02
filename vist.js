6e907d5a3315f2a6d66c74d738f388fd4d75d50a
bubbleViki <bubble_viki@163.com>
2017-03-02 11:00:58 +0800

    Add files via upload

---
+++ vist.js
@@ -0,0 +1,14 @@
+document.addEventListener('keydown', vist); //add an event listener that is waiting for a key down event.
+
+function vist(e){
+	var x=e.keyCode;
+	
+	if(window.getSelection){
+		var content=window.getSelection().toString();
+		if(x == 13){
+			var search=window.open("http://www.google.com/#q="+content+"&*");//前面应写明路径，绝对路径(http://)和相对路径(../)
+	
+		}
+		
+	}
+}
