7256c2d77b0fe2b4119deed66dcc23bfdd03e5a7
bubbleViki <bubble_viki@163.com>
2017-05-05 18:10:56 +0800

    fixed the Enter problem(no selected words but still jumped to the search page)

--- vist.js
+++ vist.js
@@ -3,12 +3,16 @@ document.addEventListener('keydown', vist); //add an event listener that is wait
 function vist(e){
 	var x=e.keyCode;
 	
+	
 	if(window.getSelection){
 		var content=window.getSelection().toString();
-		if(x == 13){
+			
+		if(x == 13 && content.length>=1){
 			var search=window.open("http://www.google.com/#q="+content+"&*");//前面应写明路径，绝对路径(http://)和相对路径(../)
 	
 		}
 		
 	}
-}
+
+	
+	}
